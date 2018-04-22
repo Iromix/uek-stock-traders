f9f165d151d845cc2754f346951e396700a3c2eb
iromix <djiro@interia.eu>
2018-04-22 14:56:27 +0200

    stock charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,21 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
 
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import stock from 'highcharts/modules/stock.src.js';
+import more from 'highcharts/highcharts-more.src.js';
+
 import { AuthService } from '../services/auth.service';
 import { NgxErrorsModule } from '@ultimate/ngxerrors';
 import { Network } from '@ionic-native/network';
 import { HttpClientModule } from '@angular/common/http';
 import { StockDataService } from './stocks/stocks-data.service';
 import { UserStocksService } from '../services/user-stocks.service';
+import { StockChartComponent } from './stock-chart/stock-chart.component';
+
+export function highchartsModules() {
+    return [more, stock];
+}
 
 export const firebaseConfig = {
     apiKey: 'aaa',
@@ -37,6 +46,7 @@ export const firebaseConfig = {
         HomePage,
         LoginPage,
         SignupPage,
+        StockChartComponent,
     ],
     imports: [
         BrowserModule,
@@ -47,6 +57,7 @@ export const firebaseConfig = {
         AngularFirestoreModule,
         AngularFireAuthModule,
         HttpClientModule,
+        ChartModule,
     ],
     bootstrap: [IonicApp],
     entryComponents: [
@@ -54,8 +65,10 @@ export const firebaseConfig = {
         HomePage,
         LoginPage,
         SignupPage,
+        StockChartComponent,
     ],
     providers: [
+        { provide: HIGHCHARTS_MODULES, useFactory: highchartsModules },
         SplashScreen,
         AngularFireDatabase,
         AngularFireAuth,
