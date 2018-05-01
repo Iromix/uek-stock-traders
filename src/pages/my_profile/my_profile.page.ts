f2ad1e77116a143915de208f34b7c8aea9e23b04
mateuszkozlowski <mateusz.kozlowski@gmail.com>
2018-05-01 20:07:37 +0200

    firebase config

--- src/pages/my_profile/my_profile.page.ts
+++ src/pages/my_profile/my_profile.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HomePage } from '../home/home.page';
+import * as firebase from 'firebase';
 
 @Component({
     templateUrl: 'my_profile.page.html',
@@ -8,10 +9,20 @@ import { HomePage } from '../home/home.page';
 export class MyProfilePage {
     public user: any;
 
-    constructor(private navCtrl: NavController) {}
+    constructor(private navCtrl: NavController){}
 
-    private openHomePage() {
+    private home_page() {
         this.navCtrl.popToRoot();
     }
+    
 }
 
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+
+if (user != null) {
+  name = user.displayName;
+  email = user.email;
+  photoUrl = user.photoURL;
+  emailVerified = user.emailVerified;
+}
