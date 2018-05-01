2929ab8eed91faf2db72bff285f87be9a30d7291
mateuszkozlowski <mateusz.kozlowski@gmail.com>
2018-05-01 21:17:06 +0200

    profile photo and name added

--- src/pages/my_profile/my_profile.page.ts
+++ src/pages/my_profile/my_profile.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HomePage } from '../home/home.page';
+import * as firebase from 'firebase';
 
 @Component({
     templateUrl: 'my_profile.page.html',
@@ -14,4 +15,22 @@ export class MyProfilePage {
         this.navCtrl.popToRoot();
     }
 }
+var config = {
+    apiKey: 'AIzaSyCUMLEvdZ72PXAQJrbG6E3oJlgX-i_zGGs',
+    authDomain: 'uek-stock-traders.firebaseapp.com',
+    databaseURL: 'https://uek-stock-traders.firebaseio.com',
+    projectId: 'uek-stock-traders',
+    storageBucket: 'uek-stock-traders.appspot.com',
+    messagingSenderId: '574461787753'
+  };
 
+firebase.initializeApp(config);
+
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+
+if (user != null) {
+  name = user.displayName;
+  email = user.email;
+  photoUrl = user.photoURL;
+}
