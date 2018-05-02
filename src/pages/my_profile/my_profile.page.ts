61a45e364b77ff3e33de9324db081abd31843382
iromix <djiro@interia.eu>
2018-05-02 16:31:52 +0200

    display user data in my profile

--- src/pages/my_profile/my_profile.page.ts
+++ src/pages/my_profile/my_profile.page.ts
@@ -1,36 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { HomePage } from '../home/home.page';
 import * as firebase from 'firebase';
 
 @Component({
     templateUrl: 'my_profile.page.html',
 })
 export class MyProfilePage {
-    public user: any;
+    private name: string;
+    private email: string;
+    private photoUrl: string;
 
-    constructor(private navCtrl: NavController) {}
+    constructor(private navCtrl: NavController) {
+        const user = firebase.auth().currentUser;
+
+        if (user != null) {
+            this.name = user.displayName;
+            this.email = user.email;
+            this.photoUrl = user.photoURL;
+        }
+    }
 
     private openHomePage() {
         this.navCtrl.popToRoot();
     }
-}
-var config = {
-    apiKey: 'AIzaSyCUMLEvdZ72PXAQJrbG6E3oJlgX-i_zGGs',
-    authDomain: 'uek-stock-traders.firebaseapp.com',
-    databaseURL: 'https://uek-stock-traders.firebaseio.com',
-    projectId: 'uek-stock-traders',
-    storageBucket: 'uek-stock-traders.appspot.com',
-    messagingSenderId: '574461787753'
-  };
-
-firebase.initializeApp(config);
-
-var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid, emailVerified;
-
-if (user != null) {
-  name = user.displayName;
-  email = user.email;
-  photoUrl = user.photoURL;
 }
