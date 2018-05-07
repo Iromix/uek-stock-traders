0a9715d7887d44c3b0610bd94a788a934d13dd1e
iromix <djiro@interia.eu>
2018-05-07 21:50:23 +0200

    set delete or add button when stock is in wallet or not

--- src/pages/stock_chart/stock_chart.page.ts
+++ src/pages/stock_chart/stock_chart.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ToastController} from 'ionic-angular';
-import { HomePage } from '../home/home.page';
 import { UserStocksService } from '../../services/user-stocks.service';
 import { StockDataService } from '../../app/stocks/stocks-data.service';
+import { StockQuote } from '../../app/stocks/stock-quote.model';
 
 @Component({
     templateUrl: 'stock_chart.page.html',
 })
 export class StockChartPage {
+
+    alreadyInWallet: boolean = false;
     private companySymbol: string = '';
     private companyName: string = '';
 
@@ -15,6 +17,13 @@ export class StockChartPage {
                 private stockService: UserStocksService) {
         this.companySymbol = this.navParams.get('company_symbol');
         this.companyName = this.navParams.get('company_name');
+
+        this.stockService.loadStockWallet();
+        this.stockService.stockQuotes.subscribe((stock: StockQuote[]) => {
+            if (stock.find(x => x.symbol == this.companySymbol)) {
+                this.alreadyInWallet = true;
+            }
+        });
     }
 
     private openHomePage() {
@@ -31,11 +40,13 @@ export class StockChartPage {
 
     private addStockToWallet(symbol: string) {
         this.stockService.getStockFromAPIAndAddToWallet(symbol);
+        this.alreadyInWallet = true;
         this.showToast('Stock added to wallet');
     }
 
     private deleteStock(symbol: string) {
         this.stockService.deleteStockFromWallet(symbol);
+        this.alreadyInWallet = false;
         this.showToast('Stock deleted from wallet');
     }
 }
