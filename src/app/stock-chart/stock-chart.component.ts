7a6ed29ab8e06436ded599d259d480742b60691e
iromix <djiro@interia.eu>
2018-05-09 20:50:46 +0200

    show price in chart title

--- src/app/stock-chart/stock-chart.component.ts
+++ src/app/stock-chart/stock-chart.component.ts
@@ -3,6 +3,7 @@ import { StockChart } from 'angular-highcharts';
 import { StockDataService } from '../stocks/stocks-data.service';
 import { DateRange } from '../stocks/data-range.model';
 import { Subscription } from 'rxjs';
+import {StockQuote} from '../stocks/stock-quote.model';
 
 @Component({
   selector: 'ib-stock-chart',
@@ -13,6 +14,7 @@ export class StockChartComponent implements OnInit, OnDestroy {
     @Input() private stockSymbol: string;
     private showLoading: boolean = true;
     private chart: StockChart;
+    private stockQuote: StockQuote;
     private stockQuoteSubscription: Subscription;
     private chartDataSubscription: Subscription;
 
@@ -35,6 +37,7 @@ export class StockChartComponent implements OnInit, OnDestroy {
 
     private initChart() {
         this.stockQuoteSubscription = this.stockData.getStockQuote(this.stockSymbol).subscribe((stock) => {
+            this.stockQuote = stock;
             this.chartDataSubscription = this.stockData.getStockDataForChart(this.stockSymbol, DateRange.FiveYears).subscribe((chartData) => {
                     const chartDataFormatted: any = [];
                     chartData.forEach((e) => {
@@ -46,9 +49,6 @@ export class StockChartComponent implements OnInit, OnDestroy {
                     this.chart = new StockChart({
                         rangeSelector: {
                             selected: 1
-                        },
-                        title: {
-                            text: stock.companyName
                         },
                         series: [{
                             name: stock.symbol,
